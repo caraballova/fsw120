9ef7ae2ea0c16e1411c18e279ac0d02af5d02a43
caraballova <vanessa.caraballo@bryanuniversity.edu>
2020-12-20 00:00:21 -0500

    Todo Part 1

---
+++ app.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+function App() {
+    return (
+        <div>
+            <input type="checkbox"/>
+            <p>Clean</p>
+
+            <input type="checkbox"/>
+            <p>Grocery Shopping</p>
+
+            <input type="checkbox"/>
+            <p>Homework</p>
+
+            <input type="checkbox"/>
+            <p>Make Doctor Appt</p>
+        </div>
+    )
+}
+
+export default App
